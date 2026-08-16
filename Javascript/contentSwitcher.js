6fba495c014b2f27b4618a62f6a48ef17f292d58
PurpleOlive <oliverstareosterlund@icloud.com>
2026-08-16 10:18:35 +0200

    Add and style content switchers

---
+++ Javascript/contentSwitcher.js
@@ -0,0 +1,76 @@
+
+export function BindToContentSwitchers()
+{
+    const switchers = document.querySelectorAll('.Switcher');
+
+    for(const switcher of switchers)
+    {
+        switcher.addEventListener('click', OnContentSwitcherTabClicked);
+    }
+
+    TryRecoverActiveSwitchers();
+}
+
+function OnContentSwitcherTabClicked(event)
+{
+    const contents = document.querySelectorAll('.SwitcherContent');
+
+    let tab = event.target;
+
+    SetActiveSwitcherContent(tab);
+}
+
+function TryRecoverActiveSwitchers()
+{
+    const contentSwitchers = document.querySelectorAll('.ContentSwitcher');
+
+    for(const contentSwitcher of contentSwitchers)
+    {
+        let lastActiveSwitcher = sessionStorage.getItem(contentSwitcher.id);
+        if(!lastActiveSwitcher)
+        {
+            continue;
+        }
+
+        let switchers = contentSwitcher.children[0];
+        for(const child of switchers.children)
+        {
+            let test1 = child.classList.contains("Switch");
+            let test2 = child.dataset.tab == lastActiveSwitcher;
+            if(child.classList.contains("Switch") && child.dataset.tab == lastActiveSwitcher)
+            {
+                SetActiveSwitcherContent(child);
+            }
+        }
+    }
+}
+
+function SetActiveSwitcherContent(activeSwitcher)
+{
+    let switchers = activeSwitcher.parentElement;
+    let root = switchers.parentElement;
+
+    const target = activeSwitcher.dataset.tab;
+
+    // Remove any tab actives
+    for(const element of switchers.children)
+    {        
+        element.classList.remove('active');
+    }
+
+    // Mark clicked as active
+    activeSwitcher.classList.add('active');
+
+    // Remove active class from all contents and add one to the clicked ones
+    for(const element of root.children)
+    {        
+        element.classList.remove('active');
+
+        if(element.id == target)
+        {
+            element.classList.add('active');
+        }
+    }
+
+    sessionStorage.setItem(root.id, target);
+}
--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,7 @@
 import * as contentLoader from './contentLoader.js'
 import * as navigation from './navigation.js'
 import * as dropdown from './dropdown.js'
+import * as contentSwitcher from './contentSwitcher.js'
 
 window.addEventListener('load', main);
 
@@ -8,11 +9,12 @@ function main()
 {
     window.removeEventListener('load', main);
 
+    contentSwitcher.BindToContentSwitchers();
+
     contentLoader.InsertPageElements().then(() => 
     {
         navigation.SetActiveItem();
         dropdown.ListenForDropdownPressed();
         dropdown.ListenForDisplaySizeChanged();
-        
     });
 }
